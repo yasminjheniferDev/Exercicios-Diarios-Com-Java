61e0290ca86d691724f6f18a3e527f504bf98382
YASMIN JHENIFER <155997837+yasminjheniferDev@users.noreply.github.com>
2026-09-05 20:26:52 -0300

    + 1 Exercício

---
+++ Exercicios 3/MultiplosDe7.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class MultiplosDe7 {
+    public static void main(String[] args) {
+
+        for (int cont = 0; cont <= 1001; cont+=7) {
+                System.out.println(cont);
+            }
+        }
+    }
+
