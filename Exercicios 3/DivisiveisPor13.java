ee1b3852464030b2e2ef501d4fff3d94c0e5b2aa
YASMIN JHENIFER <155997837+yasminjheniferDev@users.noreply.github.com>
2026-09-07 12:28:19 -0300

    + 1 Exercício

---
+++ Exercicios 3/DivisiveisPor13.java
@@ -0,0 +1,12 @@
+public class DivisiveisPor13 {
+    public static void main(String[] args) {
+
+        //Laço imprimindo números de 1000 a 1999, múltiplos de 13.
+        for( int cont = 1000; cont <=1999;cont++){
+            if(cont % 13 == 0){
+                System.out.println(cont);
+
+            }
+        }
+    }
+}
