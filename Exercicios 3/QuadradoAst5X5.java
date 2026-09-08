060973f0e29cfcd1bfe9ead7aa0983eaafc6f7a4
YASMIN JHENIFER <155997837+yasminjheniferDev@users.noreply.github.com>
2026-09-08 19:28:26 -0300

    + 1 Exercício

---
+++ Exercicios 3/QuadradoAst5X5.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class QuadradoAst5X5 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);{
+
+
+            for(int coluna = 1; coluna <=5; coluna++){
+                for(int linha = 1; linha <=5; linha++){
+                    System.out.print("*\t");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
